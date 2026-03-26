5606a9973fd57075bf20526959a3e0fd91d40561
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-03-26 17:51:50 -0400

    fix fog

--- src/main/java/dev/vesper/palegardenfx/mixin/FogRendererMixin.java
+++ src/main/java/dev/vesper/palegardenfx/mixin/FogRendererMixin.java
@@ -5,6 +5,7 @@ import dev.vesper.eveningstarlib.common.ESLModChecks;
 import dev.vesper.palegardenfx.common.Config;
 import net.minecraft.client.Camera;
 import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.fog.FogData;
 import net.minecraft.client.renderer.fog.FogRenderer;
@@ -22,6 +23,7 @@ import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -30,72 +32,31 @@ import static dev.vesper.palegardenfx.common.FogStateManager.fogFade;
 @Mixin(FogRenderer.class)
 public class FogRendererMixin {
 
-    private static final float FADE_SPEED = 0.002f;
     private static float fogAlphaBase;
-    private static float fogStart;
-    private static float fogEnd;
+    private static Entity entity;
+    private static float renderDistanceBlocks;
 
-    /*@Inject(method = "setupFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;getDevice()Lcom/mojang/blaze3d/systems/GpuDevice;", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private void afterLoadLevel(Camera camera, int i, DeltaTracker deltaTracker, float f, ClientLevel world, CallbackInfoReturnable<Vector4f> cir, float tickProgress, Vector4f color, float renderDistanceBlocks, @Local Entity entity, @Local FogData fogData) {
-        if (entity instanceof Player player) {
-            if (!ESLModChecks.isShaders()) {
-                BlockPos pos = player.getOnPos();
-                Holder<@NotNull Biome> biome = world.getBiome(pos);
-                if (!biome.is(Biomes.PALE_GARDEN)) {
-                    return;
-                }
+    @Inject(method = "setupFog", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/fog/environment/FogEnvironment;setupFog(Lnet/minecraft/client/renderer/fog/FogData;Lnet/minecraft/client/Camera;Lnet/minecraft/client/multiplayer/ClientLevel;FLnet/minecraft/client/DeltaTracker;)V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void onFogStart(Camera camera, int renderDistanceInChunks, DeltaTracker deltaTracker, float darkenWorldAmount, ClientLevel level, CallbackInfoReturnable<FogData> cir, @Local(name = "entity") Entity localEntity) {
+        entity = localEntity;
+        renderDistanceBlocks = renderDistanceInChunks * 16;
+    }
 
-                int topY = world.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ());
-                if (biome.is(Biomes.PALE_GARDEN)) {
-                    if (player.getY() <= (double)(topY + 15) && player.getY() >= 15.0D && !player.isCreative() && !player.isSpectator()) {
-                        fogFade = Math.min(fogFade + 0.002F, 1.0F);
-                    } else {
-                        if (!(fogFade > 0.0F)) {
-                            return;
-                        }
-                        fogFade = Math.min(fogFade - 0.002F, 0.0F);
-                    }
-                }
-
-                if (Config.horrorMode) {
-                    fogData.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (0.1F - renderDistanceBlocks * 0.8F);
-                    fogData.environmentalEnd = renderDistanceBlocks + fogFade * (8.0F - renderDistanceBlocks);
-                    fogAlphaBase = 0.99F;
-                } else {
-                    fogData.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (Config.fogStart - renderDistanceBlocks * 0.8F);
-                    fogData.environmentalEnd = renderDistanceBlocks + fogFade * (Config.fogEnd - renderDistanceBlocks);
-                    fogAlphaBase = Config.fogTransparency;
-                }
-
-                fogData.skyEnd = fogData.environmentalEnd;
-                fogData.cloudEnd = fogData.environmentalEnd;
-                color.x += fogFade * (0.8F - color.x);
-                color.y += fogFade * (0.8F - color.y);
-                color.z += fogFade * (0.85F - color.z);
-                color.w += fogFade * (fogAlphaBase - color.w);
-            }
-
-        }
-    }*/
-
-    @Inject(method = "setupFog", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/fog/environment/FogEnvironment;setupFog(Lnet/minecraft/client/renderer/fog/FogData;Lnet/minecraft/client/Camera;Lnet/minecraft/client/multiplayer/ClientLevel;FLnet/minecraft/client/DeltaTracker;)V", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    private static  void onFogStart(Camera camera, int renderDistanceInChunks, DeltaTracker deltaTracker, float darkenWorldAmount, ClientLevel level, CallbackInfoReturnable<FogData> cir, @Local(name = "entity") Entity entity, @Local(name = "fog") FogData fogData) {
+    @Inject(method = "updateBuffer(Lnet/minecraft/client/renderer/fog/FogData;)V", at = @At("HEAD"))
+    private void updateBuffer(FogData fog, CallbackInfo ci) {
         if (entity instanceof Player player) {
             //? fabric{
             if (!ESLModChecks.isShaders()) {
-                //?}
-                //? neoforge{
+                //?} neoforge{
                 /*if (!ModList.get().isLoaded("iris")) {
                 *///?}
                 BlockPos pos = player.getOnPos();
-                Holder<@NotNull Biome> biome = level.getBiome(pos);
-                if (!biome.is(Biomes.PALE_GARDEN)) {
-                    return;
-                }
+                assert Minecraft.getInstance().level != null;
+                Holder<@NotNull Biome> biome = Minecraft.getInstance().level.getBiome(pos);
+                if (!biome.is(Biomes.PALE_GARDEN)) {return;}
 
-                float renderDistanceBlocks = renderDistanceInChunks * 16;
+                int topY = Minecraft.getInstance().level.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ());
 
-                int topY = level.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ());
                 if (biome.is(Biomes.PALE_GARDEN)) {
                     if (player.getY() <= (double)(topY + 15) && player.getY() >= 15.0D && !player.isCreative() && !player.isSpectator()) {
                         fogFade = Math.min(fogFade + 0.002F, 1.0F);
@@ -108,21 +69,21 @@ public class FogRendererMixin {
                 }
 
                 if (Config.horrorMode) {
-                    fogData.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (0.1F - renderDistanceBlocks * 0.8F);
-                    fogData.environmentalEnd = renderDistanceBlocks + fogFade * (8.0F - renderDistanceBlocks);
+                    fog.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (0.1F - renderDistanceBlocks * 0.8F);
+                    fog.environmentalEnd = renderDistanceBlocks + fogFade * (8.0F - renderDistanceBlocks);
                     fogAlphaBase = 0.99F;
                 } else {
-                    fogData.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (Config.fogStart - renderDistanceBlocks * 0.8F);
-                    fogData.environmentalEnd = renderDistanceBlocks + fogFade * (Config.fogEnd - renderDistanceBlocks);
+                    fog.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (Config.fogStart - renderDistanceBlocks * 0.8F);
+                    fog.environmentalEnd = renderDistanceBlocks + fogFade * (Config.fogEnd - renderDistanceBlocks);
                     fogAlphaBase = Config.fogTransparency;
                 }
 
-                fogData.skyEnd = fogData.environmentalEnd;
-                fogData.cloudEnd = fogData.environmentalEnd;
-                fogData.color.x += fogFade * (0.8F - fogData.color.x);
-                fogData.color.y += fogFade * (0.8F - fogData.color.y);
-                fogData.color.z += fogFade * (0.85F - fogData.color.z);
-                fogData.color.w += fogFade * (fogAlphaBase - fogData.color.w);
+                fog.skyEnd = fog.environmentalEnd;
+                fog.cloudEnd = fog.environmentalEnd;
+                fog.color.x += fogFade * (0.8F - fog.color.x);
+                fog.color.y += fogFade * (0.8F - fog.color.y);
+                fog.color.z += fogFade * (0.85F - fog.color.z);
+                fog.color.w += fogFade * (fogAlphaBase - fog.color.w);
             }
         }
     }
