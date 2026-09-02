9a0c9a90d80c9380d98c967c1ebbf639c91b7f63
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-09-02 10:54:35 -0400

    some organization

--- src/main/java/dev/vesper/palegardenfx/common/FogCode.java
+++ src/main/java/dev/vesper/palegardenfx/common/FogCode.java
@@ -1,6 +1,5 @@
 package dev.vesper.palegardenfx.common;
 
-import dev.vesper.eveningstarlib.common.ESLModChecks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.fog.FogData;
 import net.minecraft.core.BlockPos;
@@ -16,47 +15,45 @@ public class FogCode {
 
 	//~ if 1.21.11 'float renderDistanceBlocks, FogData fog, float fogAlphaBase, Player player' -> 'float renderDistanceBlocks, FogData fog, float fogAlphaBase, Player player, Vector4f color'
 	public static void setFogBuffer(float renderDistanceBlocks, FogData fog, float fogAlphaBase, Player player) {
-		if (!ESLModChecks.isShaders()) {
-			if (Config.fogType == Config.FogType.VANILLA) {
-				BlockPos pos = player.getOnPos();
-				assert Minecraft.getInstance().level != null;
-				Holder<Biome> biome = Minecraft.getInstance().level.getBiome(pos);
-				if (!biome.is(Biomes.PALE_GARDEN)) {
-					return;
-				}
-
-				int topY = Minecraft.getInstance().level.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ());
-
-				if (biome.is(Biomes.PALE_GARDEN)) {
-					if (player.getY() <= (double) (topY + 15) && player.getY() >= 15.0D) {
-						fogFade = Math.min(fogFade + 0.002F, 1.0F);
-					} else {
-						if (!(fogFade > 0.0F)) {
-							return;
-						}
-						fogFade = Math.min(fogFade - 0.002F, 0.0F);
-					}
-				}
-
-				if (Config.horrorMode) {
-					fog.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (0.1F - renderDistanceBlocks * 0.8F);
-					fog.environmentalEnd = renderDistanceBlocks + fogFade * (8.0F - renderDistanceBlocks);
-					fogAlphaBase = 0.99F;
-				} else {
-					fog.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (Config.fogStart - renderDistanceBlocks * 0.8F);
-					fog.environmentalEnd = renderDistanceBlocks + fogFade * (Config.fogEnd - renderDistanceBlocks);
-					fogAlphaBase = Config.fogTransparency;
-				}
-
-				fog.skyEnd = fog.environmentalEnd;
-				fog.cloudEnd = fog.environmentalEnd;
-				//~ if 1.21.11 'fog.color' -> 'color' {
-				fog.color.x += fogFade * (0.8F - fog.color.x);
-				fog.color.y += fogFade * (0.8F - fog.color.y);
-				fog.color.z += fogFade * (0.85F - fog.color.z);
-				fog.color.w += fogFade * (fogAlphaBase - fog.color.w);
-				//~}
+		if (Config.fogType == Config.FogType.VANILLA) {
+			BlockPos pos = player.getOnPos();
+			assert Minecraft.getInstance().level != null;
+			Holder<Biome> biome = Minecraft.getInstance().level.getBiome(pos);
+			if (!biome.is(Biomes.PALE_GARDEN)) {
+				return;
 			}
+
+			int topY = Minecraft.getInstance().level.getHeight(Heightmap.Types.WORLD_SURFACE, pos.getX(), pos.getZ());
+
+			if (biome.is(Biomes.PALE_GARDEN)) {
+				if (player.getY() <= (double) (topY + 15) && player.getY() >= 15.0D) {
+					fogFade = Math.min(fogFade + 0.002F, 1.0F);
+				} else {
+					if (!(fogFade > 0.0F)) {
+						return;
+					}
+					fogFade = Math.min(fogFade - 0.002F, 0.0F);
+				}
+			}
+
+			if (Config.horrorMode) {
+				fog.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (0.1F - renderDistanceBlocks * 0.8F);
+				fog.environmentalEnd = renderDistanceBlocks + fogFade * (8.0F - renderDistanceBlocks);
+				fogAlphaBase = 0.99F;
+			} else {
+				fog.environmentalStart = renderDistanceBlocks * 0.8F + fogFade * (Config.fogStart - renderDistanceBlocks * 0.8F);
+				fog.environmentalEnd = renderDistanceBlocks + fogFade * (Config.fogEnd - renderDistanceBlocks);
+				fogAlphaBase = Config.fogTransparency;
+			}
+
+			fog.skyEnd = fog.environmentalEnd;
+			fog.cloudEnd = fog.environmentalEnd;
+			//~ if 1.21.11 'fog.color' -> 'color' {
+			fog.color.x += fogFade * (0.8F - fog.color.x);
+			fog.color.y += fogFade * (0.8F - fog.color.y);
+			fog.color.z += fogFade * (0.85F - fog.color.z);
+			fog.color.w += fogFade * (fogAlphaBase - fog.color.w);
+			//~}
 		}
 	}
 }
