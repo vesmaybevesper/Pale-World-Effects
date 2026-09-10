aabd78d6ba030624e27710092cbeba06f889c54a
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-09-10 15:41:12 -0400

    26.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,7 @@ stonecutter {
 		fun match(version: String, vararg loaders: String) =
 			loaders.forEach { version("$version-$it", version).buildscript = "build.$it.gradle.kts" }
 
+		match("26.3", "fabric", "neoforge")
 		match("26.2", "fabric", "neoforge")
 		match("26.1.2", "fabric", "neoforge")
 		match("1.21.11", "fabric", "neoforge")
